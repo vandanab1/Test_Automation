e7fd8016bed8f03f4a3e0967c1e29cc6db1ab47a
sharma.vishal1205 <sharma.vishal1205@gmail.com>
2026-03-13 01:01:35 +0530

    Trigger CI pipeline run

---
+++ tests/login.spec.js
@@ -0,0 +1,24 @@
+const {test, expect}= require('@playwright/test')
+
+test('login',async({page})=>{
+
+await page.goto('https://www.saucedemo.com/');
+await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+await page.locator('#user-name').fill('standard_user');
+await page.locator('#password').fill('secret_sauce');
+await page.locator('#login-button').click();
+await page.locator('#add-to-cart-sauce-labs-bolt-t-shirt').click();
+await page.locator('.shopping_cart_link').click();
+await page.locator('#checkout').click();
+await page.getByPlaceholder('First Name').fill('test');
+await page.getByPlaceholder('Last Name').fill('user');
+await page.getByPlaceholder('Zip/Postal Code').fill('200001');
+await page.locator('#continue').click();
+await page.locator('#finish').click();
+await page.locator('.btn.btn_primary.btn_small').click();
+await page.locator('#react-burger-menu-btn').click();
+await page.locator('#logout_sidebar_link').click();
+//test
+
+})
