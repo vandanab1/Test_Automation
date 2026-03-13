465b87f94e0b47ae1abf958d359f741060afbf18
vandanab1 <vandanasharma.b1@gmail.com>
2026-03-13 16:11:41 +0530

    updated test case

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -8,7 +8,7 @@ await expect(page).toHaveURL('https://www.saucedemo.com/');
 await page.locator('#user-name').fill('standard_user');
 await page.locator('#password').fill('secret_sauce');
 await page.locator('#login-button').click();
-//await page.toHaveTitle('Products');
+await page.toHaveTitle('Products');
 await page.toHaveURL('https://www.saucedemo.com/inventory.html');
 
 
